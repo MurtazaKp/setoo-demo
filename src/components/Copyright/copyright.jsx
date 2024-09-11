4df2c062be9519960593fa9d4edaa9ce2cf761bb
murtazakp <khopoliwala52@gmail.com>
2024-09-11 17:40:30 +0530

    feat(initial) : work on contact form

---
+++ src/components/Copyright/copyright.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Copyright = () => {
+  return <div className='bg-black text-white p-1 mt-auto text-center'>Copyright @Setoo Solutions – 2024 | All rights reserved.</div>;
+}
+
+export default Copyright
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="bg-gray-900 p-5">
+      <img className="w-40 " src="/logo.svg" />
+    </div>
+  );
+}
+
+export default Header
