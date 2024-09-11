607cc7bdcd5857ae51922380791923804de0c7e0
murtazakp <khopoliwala52@gmail.com>
2024-09-11 19:01:36 +0530

    feat(work) : work on dark mode

--- src/components/Contact/contact-form.jsx
+++ src/components/Contact/contact-form.jsx
@@ -24,7 +24,7 @@ const ContactForm = () => {
       phone: `${formData.phoneNumber}`,
     })
       .then((res) => {
-        if (res.status == 201) {
+        if (res.status === 201) {
           toast.success(res.message);
           setIsLoading(false);
           reset();
@@ -37,23 +37,18 @@ const ContactForm = () => {
   };
 
   return (
-    <section className="bg-gray-50 h-screen dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <section className="bg-gray-50 min-h-screen overflow-auto">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:min-h-screen lg:py-0">
         <a
           href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
         >
-          <img
-            className=" w-32  mr-2"
-            src="/logo.svg"
-            alt="logo"
-          />
-        
+          <img className="w-32 mr-2" src="/logo.svg" alt="logo" />
         </a>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-semibold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-             Submit your details 
+            <h1 className="text-xl font-semibold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Submit your details
             </h1>
             <form
               className="space-y-4 md:space-y-6"
@@ -62,7 +57,7 @@ const ContactForm = () => {
               <div>
                 <label
                   htmlFor="fullName"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Full name
                 </label>
@@ -70,7 +65,7 @@ const ContactForm = () => {
                   type="text"
                   id="fullName"
                   placeholder="Full name"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   {...register("fullName", {
                     required: "Full Name is required",
                   })}
@@ -85,7 +80,7 @@ const ContactForm = () => {
               <div>
                 <label
                   htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Your email
                 </label>
@@ -93,7 +88,7 @@ const ContactForm = () => {
                   type="email"
                   id="email"
                   placeholder="name@company.com"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   {...register("email", {
                     required: "Email is required",
                     pattern: {
@@ -113,7 +108,7 @@ const ContactForm = () => {
               <div>
                 <label
                   htmlFor="phoneNumber"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Phone number
                 </label>
@@ -132,7 +127,7 @@ const ContactForm = () => {
                     <PhoneInput
                       {...field}
                       defaultCountry="in"
-                      className="block w-full  px-2.5 border border-gray-300 text-gray-900 bg-gray-50 rounded-lg focus:ring-primary-600 focus:border-primary-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="block w-full px-2.5 border border-gray-300 text-gray-900 bg-gray-50 rounded-lg focus:ring-primary-600 focus:border-primary-600"
                       onChange={(value) => field.onChange(value)}
                     />
                   )}
@@ -146,12 +141,31 @@ const ContactForm = () => {
 
               <button
                 type="submit"
-                className="w-full text-white bg-[#2563eb] hover:bg-[#1d4ed8] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full text-white bg-[#2563eb] hover:bg-[#1d4ed8] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 disabled={isLoading}
               >
-                {isLoading ? "Loading..." : "Sign in"}
+                {isLoading && (
+                  <svg
+                    width={24}
+                    aria-hidden="true"
+                    role="status"
+                    className="inline w-4 h-4 me-3 text-white animate-spin"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="#E5E7EB"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                )}
+                Submit
               </button>
-            
             </form>
           </div>
         </div>
